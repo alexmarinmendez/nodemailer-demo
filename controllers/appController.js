0e1c08cc2c023313496d9763a1f470660a84d879
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-05 07:23:38 -0500

    feat: add nodemailer

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,5 +1,31 @@
-const signup = (req, res) => {
-    res.status(201).json("Signup successfully...")
+const nodemailer = require("nodemailer");
+
+const signup = async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      });
+
+    let message = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "alexmarinmendez@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      };
+
+    transporter.sendMail(message)
+      .then(() => {
+        return res.status(201).json({ msg: "you should receive an email"})
+      })
+      .catch(err => res.status(500).json({ err }))
+
 }
 
 const getbill = (req, res) => {
